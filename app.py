57847287a9ec864efc34e99c1e1761835865efb0
AravindVasudev <aravindv96@yahoo.com>
2017-02-05 03:01:19 +0530

    Tweets everytime when mentioned

--- app.py
+++ app.py
@@ -8,16 +8,23 @@ import time
 import tweepy
 
 class UserStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+
     def on_status(self, status):
-        print(status.text)
+        try:
+            self.api.update_status('Hey @{}! What\'s up?'.format(status.user.screen_name))
+            print('Tweeted To: @{}'.format(status.user.screen_name))
+        except Exception as e:
+            print('Error Occured: {}'.format(e))
 
     def on_error(self, status_code):
         print('Got an error with status code: ' + str(status_code))
-        return True # To continue listening
+        return False # exit
 
     def on_timeout(self):
         print('Timeout...')
-        return True # To continue listening
+        return False # exit
 
 def main():
     # Authentication
@@ -26,11 +33,10 @@ def main():
     api = tweepy.API(auth)
 
     # Create the Stream
-    userTweetStream = tweepy.Stream(auth=api.auth, listener=UserStreamListener())
+    userTweetStream = tweepy.Stream(auth=api.auth, listener=UserStreamListener(api))
 
     # Start the Stream
-    userTweetStream.userstream()
-
+    userTweetStream.filter(track=['@' + api.me().screen_name])
 
 
 if __name__ == '__main__': main()
